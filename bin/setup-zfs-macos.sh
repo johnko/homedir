712e2da2e344ba909bc3f3202a8c8dc13f7ff4ac
johnko <git@johnko.ca>
2018-03-18 02:29:42 -0400

    create enc, data fs

--- bin/setup-zfs-macos.sh
+++ bin/setup-zfs-macos.sh
@@ -13,17 +13,35 @@ create_default_zpool ()
         -O copies=2 \
         ${poolname} ${diskX}
     # mirror $diskX $diskY
+    while sudo mdutil -i off /Volumes/${poolname} | grep 'Please try again in a moment'; do
+        sleep 1
+    done
     sudo zpool set feature@encryption=enabled ${poolname}
 }
 
-create_data_filesystem ()
+create_enc_filesystem ()
 {
     sudo zfs create \
         -o encryption=on -o keylocation=prompt -o keyformat=passphrase \
         -o casesensitivity=sensitive \
         -o normalization=formD \
         ${poolname}/enc
+    while sudo mdutil -i off /Volumes/${poolname}/enc | grep 'Please try again in a moment'; do
+        sleep 1
+    done
+}
+
+create_data_filesystem ()
+{
+    sudo zfs create \
+        -o casesensitivity=sensitive \
+        -o normalization=formD \
+        ${poolname}/data
+    while sudo mdutil -i off /Volumes/${poolname}/data | grep 'Please try again in a moment'; do
+        sleep 1
+    done
 }
 
 create_default_zpool
+create_enc_filesystem
 create_data_filesystem
