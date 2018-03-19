480c7f802a29d0341166298d345514bb1b7a1fcc
johnko <git@johnko.ca>
2018-03-18 20:02:06 -0400

    map to .git2 since .git should be read-write

--- bin/docker-ubuntu-16.04.sh
+++ bin/docker-ubuntu-16.04.sh
@@ -7,7 +7,7 @@ MY_DOCKER_IMAGE=ubuntu:16.04
 
 docker pull ${MY_DOCKER_IMAGE}
 if [ -e ${HOME}/.git ]; then
-    MY_VOLUME_ARGS="--volume ${HOME}/.git:/root/.git:ro"
+    MY_VOLUME_ARGS="--volume ${HOME}/.git:/root/.git2:ro"
 else
     MY_VOLUME_ARGS=""
 fi
