6b6db0b3b713a0e3c88e7751d807ed199933b8dd
johnko <git@johnko.ca>
2018-03-18 23:04:00 -0400

    use docker-compose.yml and localhost

--- bin/jenkins-lts.sh
+++ bin/jenkins-lts.sh
@@ -13,21 +13,22 @@ docker pull ${MY_DOCKER_IMAGE}
 
 [ ! -d ${MY_TMP_CONTEXT} ] && mkdir ${MY_TMP_CONTEXT}
 cd ${MY_TMP_CONTEXT}
-cat > jenkins-lts.yml <<EOF
+cat > docker-compose.yml <<EOF
 version: "3.5"
 
 services:
   myjenkins:
     image: ${MY_DOCKER_IMAGE}
     ports:
-      - 8080:8080
-      - 50000:50000
+      - 127.0.0.1:8080:8080
+      - 127.0.0.1:50000:50000
     volumes:
       - jenkins_home:/var/jenkins_home
 
 volumes:
   jenkins_home:
 EOF
+
 ## Create and run the stack interactively
 # docker-compose up
 ## Create and run the stack in the background
@@ -37,5 +38,6 @@ docker-compose up -d
 # docker-compose down --volumes
 ## Destroy the stack but keep the data
 # docker-compose down
+
 cd -
 rm -r ${MY_TMP_CONTEXT}
