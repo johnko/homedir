3393fbde1ec3c40b542aced0e05bf8a66a84a64b
johnko <git@johnko.ca>
2018-08-30 15:22:25 -0400

    sync with ubuntu-zfs

--- bin/setup-ansible.sh
+++ bin/setup-ansible.sh
@@ -1,43 +1,42 @@
 #!/usr/bin/env bash
 set -e
 set -x
-set -u
 
 if which apt-get; then
-    export DEBIAN_FRONTEND=noninteractive
-    ## Dependency for virtualenv
-    sudo apt-get install --yes build-essential python-dev libffi-dev
-    which virtualenv || sudo apt-get install --yes python-virtualenv
-    ## Dependency for ansible
-    sudo apt-get install --yes libssl-dev
+  export DEBIAN_FRONTEND=noninteractive
+  # Dependency for virtualenv
+  sudo apt-get install --yes build-essential python-dev libffi-dev
+  which virtualenv || sudo apt-get install --yes python-virtualenv
+  # Dependency for ansible
+  sudo apt-get install --yes libssl-dev
 fi
 
 if ! which virtualenv; then
-    if which pip2; then
-        pip2 install virtualenv
-    fi
+  if which pip2; then
+    pip2 install virtualenv
+  fi
 fi
 
 if ! which virtualenv; then
-    if which pip; then
-        pip install virtualenv
-    fi
+  if which pip; then
+    pip install virtualenv
+  fi
 fi
 
 if ! which virtualenv; then
-    if which easy_install; then
-        sudo easy_install virtualenv
-    fi
+  if which easy_install; then
+    sudo easy_install virtualenv
+  fi
 fi
 
-## Install ansible in a virtualenv
+# Install ansible in a virtualenv
 VENV_FOLDER="venv"
 virtualenv ${VENV_FOLDER}
 . ${VENV_FOLDER}/bin/activate
 pip2 install --upgrade pip
 pip2 install --upgrade ansible
 
-## Instructions to user
+# Instructions to user
 set +x
 echo "======================================"
 echo "Ansible setup complete!"
