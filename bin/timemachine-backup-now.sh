caa9423639524b73f5b2b6d7c996a4b43a2b4292
johnko <git@johnko.ca>
2019-02-24 00:19:50 -0500

    don't use auto for tmutil

--- bin/timemachine-backup-now.sh
+++ bin/timemachine-backup-now.sh
@@ -3,7 +3,10 @@ set -e
 set -x
 set -u
 
+# Mac OS Extended (Case-sensitive, Journaled, Encrypted)
+
 for id in $(tmutil destinationinfo | grep "^ID" | awk '{print $3}'); do
-  tmutil startbackup --auto --block --rotation --destination $id
+  #tmutil startbackup --auto --block --rotation --destination $id
+  tmutil startbackup --block --rotation --destination $id
 done
 
