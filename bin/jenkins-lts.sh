a54afba86d5c5569d02f641c8a22186d20caf780
johnko <git@johnko.ca>
2020-12-29 19:22:14 -0500

    add jenkinsagent role and rolebinding

--- bin/jenkins-lts.sh
+++ bin/jenkins-lts.sh
@@ -30,32 +30,40 @@ top)
   ;;
 k8sagents)
   set +x
+  echo "==> Automation START"
   grep certificate-authority-data ${HOME}/.kube/config | awk '{print $NF}' | base64 -d >./kind-ca.crt
 
   kubectl apply -f ./k8s-ns.yaml
 
-  [ -e ./kind-client.key ] || openssl genrsa -out ./kind-client.key 4096
-  [ -e ./kind-client.csr ] || openssl req -config ./csr.conf -new -key ./kind-client.key -nodes -out ./kind-client.csr
-  export BASE64_CSR=$(cat ./kind-client.csr | base64 | tr -d '\n')
-  cat ./k8s-csr.yaml | envsubst | kubectl apply -f -
-  kubectl get csr jenkinsagent
-  kubectl certificate approve jenkinsagent
-  [ -e ./kind-client.crt ] || kubectl get csr jenkinsagent -o jsonpath='{.status.certificate}' | base64 --decode >./kind-client.crt
-  [ -e ./kind-client.pfx ] || openssl pkcs12 -export -out ./kind-client.pfx -inkey ./kind-client.key -in ./kind-client.crt -certfile ./kind-ca.crt
-
-  echo "Jenkins > Configure Clouds"
+  if [ ! -e ./kind-client.pfx ]; then
+    [ -e ./kind-client.key ] || openssl genrsa -out ./kind-client.key 4096
+    if [ ! -e ./kind-client.csr ]; then
+      openssl req -config ./csr.conf -new -key ./kind-client.key -nodes -out ./kind-client.csr
+      export BASE64_CSR=$(cat ./kind-client.csr | base64 | tr -d '\n')
+      cat ./k8s-csr.yaml | envsubst | kubectl apply -f -
+      kubectl get csr jenkinsagent
+      kubectl certificate approve jenkinsagent
+    fi
+    [ -e ./kind-client.crt ] || kubectl get csr jenkinsagent -o jsonpath='{.status.certificate}' | base64 --decode >./kind-client.crt
+    [ -e ./kind-client.pfx ] || openssl pkcs12 -export -out ./kind-client.pfx -inkey ./kind-client.key -in ./kind-client.crt -certfile ./kind-ca.crt
+  fi
+  kubectl apply -f ./k8s-role.yaml
+  echo "==> Automation END"
+  echo
+  echo "==> Please open Jenkins > Manage Jenkins > Manage Nodes > Configure Clouds"
+  echo
   echo "==> Kubernetes URL:"
   echo "https://kindlocal-control-plane:6443"
-
+  echo
   echo "==> Kubernetes server certificate key"
   cat ./kind-ca.crt
-
+  echo
   echo "==> Kubernetes Namespace:"
   echo "jenkinsagent"
-
+  echo
   echo "==> Credentials:"
   ls -l $(pwd)/kind-client.pfx
-
+  echo
   echo "==> Jenkins URL:"
   echo "http://jenkins-2.249.1:8080"
 
