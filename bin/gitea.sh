753a3caa91f2b8cbcba22a210905c0519acab51a
johnko <git@johnko.ca>
2021-04-02 17:47:23 -0400

    ssl postgres

--- bin/gitea.sh
+++ bin/gitea.sh
@@ -6,6 +6,13 @@ MY_TMP_CONTEXT="${HOME}/docker-files/gitea"
 [ ! -d ${MY_TMP_CONTEXT} ] && exit 1
 cd ${MY_TMP_CONTEXT}
 
+generate_selfsigned_cert() {
+  if ! [ -e ./pgsql-server.key ] && ! [ -e ./pgsql-server.pem ]; then
+    openssl req -x509 -nodes -days 182 -newkey rsa:2048 -keyout ./pgsql-server.key -out ./pgsql-server.pem -config ./csr.conf -extensions 'v3_req'
+    openssl x509 -in ./pgsql-server.pem -noout -text
+  fi
+}
+
 set +u
 case ${1} in
 open)
@@ -25,15 +32,22 @@ logs)
 ps)
   docker-compose ps
   ;;
+test-new)
+  docker-compose down --volumes
+  generate_selfsigned_cert
+  docker-compose pull
+  docker-compose up --detach
+  ;;
 top)
   docker-compose top
   ;;
 up | *)
+  generate_selfsigned_cert
   docker-compose pull
   ## Create and run the stack interactively
   # docker-compose up
   ## Create and run the stack in the background
-  docker-compose up -d
+  docker-compose up --detach
   ;;
 esac
 
