952aae1bd15273f34d71b62555aafc8ef8ab3079
johnko <279736+johnko@users.noreply.github.com>
2022-01-28 22:03:15 -0500

    use venv instead of virtualenv

--- bin/setup-python3.sh
+++ bin/setup-python3.sh
@@ -16,51 +16,48 @@ if command -v apt-get >/dev/null 2>&1 ; then
   sudo apt-get install --yes libssl-dev
 fi
 
-echo "=>  Detecting pip3, pip2 or pip..."
-# Last one wins
-command -v pip  >/dev/null 2>&1 && PIP_BIN="pip"
-command -v pip3 >/dev/null 2>&1 && PIP_BIN="pip3"
+echo "=>  Detecting python3, python2..."
 # Last one wins
 command -v python    >/dev/null 2>&1 && PY_BIN="python"
 command -v python3   >/dev/null 2>&1 && PY_BIN="python3"
 command -v python3.7 >/dev/null 2>&1 && PY_BIN="python3.7"
 
-echo "=>  Installing virtualenv..."
-if ! command -v virtualenv >/dev/null 2>&1 ; then
-  if [ -n "${PIP_BIN}" ] ; then
-    ${PIP_BIN} config set global.index-url https://pypi.org/simple 2>&1 | awk '{print "    "$0}'
-    ${PIP_BIN} config list 2>&1 | awk '{print "    "$0}'
-    ${PIP_BIN} install virtualenv 2>&1 | awk '{print "    "$0}'
-  fi
-fi
-if ! command -v virtualenv >/dev/null 2>&1 ; then
-  command -v easy_install >/dev/null 2>&1 && \
-    easy_install virtualenv 2>&1 | awk '{print "    "$0}'
-fi
-if ! command -v virtualenv >/dev/null 2>&1 ; then
-  echo "!!  Failed to install virtualenv"
+VENV_FOLDER="venv-python3"
+VENV_PATH="${HOME}/${VENV_FOLDER}"
+${PY_BIN} -m venv ${VENV_PATH}
+source ${VENV_PATH}/bin/activate
+
+echo "=>  Detecting pip3, pip2 or pip..."
+# Last one wins
+command -v pip  >/dev/null 2>&1 && PIP_BIN="pip"
+command -v pip3 >/dev/null 2>&1 && PIP_BIN="pip3"
+
+if ! which python | grep -q ${VENV_FOLDER} ; then
+  echo "!!  Failed to use venv"
   exit 1
 fi
 
-# Install dev tools in a virtualenv
-VENV_FOLDER="virtualenv-py3"
-virtualenv "${VENV_FOLDER}" | awk '{print "    "$0}'
-source "${VENV_FOLDER}/bin/activate"
+echo "=>  Listing python libraries..."
+
+${PIP_BIN} list
 
 echo "=>  Installing latest pip..."
-curl -s https://bootstrap.pypa.io/get-pip.py | ${PY_BIN} 2>&1 | awk '{print "    "$0}'
+
+${PIP_BIN} install --upgrade pip setuptools wheel
 
 echo "=>  Installing python development tools..."
 ${PIP_BIN} install --upgrade \
+  pygments \
   yamllint \
   yq \
   flake8 \
   nose \
   jinjalint \
   ansible \
+  awscli \
 2>&1 | awk '{print "    "$0}'
 echo "=>  Installation complete!"
 
 echo "    To activate, run:"
-echo "        source ${VENV_FOLDER}/bin/activate"
+echo "        source ${VENV_PATH}/bin/activate"
 echo "=>  Setup is complete!"
