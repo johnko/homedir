3b3a78fe57e27929a98e58a37353c7eb18dec48d
johnko <279736+johnko@users.noreply.github.com>
2022-01-29 01:15:31 -0500

    build helm

--- bin/setup-kind.sh
+++ bin/setup-kind.sh
@@ -15,7 +15,16 @@ export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin"
 
 go get sigs.k8s.io/kind
+
 go get helm.sh/helm/v3
+HELM_SRC_PATH="$GOPATH/helmv3.8.0"
+test -e "$HELM_SRC_PATH" && chmod -R 700 "$HELM_SRC_PATH"
+test -e "$HELM_SRC_PATH" && rm -fr "$HELM_SRC_PATH"
+cp -r $GOPATH/pkg/mod/helm.sh/helm/v3@v3.8.0 "$HELM_SRC_PATH"
+chmod 700 "$HELM_SRC_PATH"
+cd "$HELM_SRC_PATH"
+make
+cp "$HELM_SRC_PATH/bin/helm" "$GOPATH/bin/"
 
 kind create cluster
 
