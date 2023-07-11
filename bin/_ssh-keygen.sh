367608ac609fb140fc70c7642ca112eb68db5062
johnko <279736+johnko@users.noreply.github.com>
2023-07-10 23:50:39 -0400

    remove old ssh-keygen script

--- bin/_ssh-keygen.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-# OUTFILE="${HOME}/.ssh/id_rsanopass"
-# if [ ! -f "${OUTFILE}" ]; then
-#   ssh-keygen -b 4096 -t rsa -N '' -f "${OUTFILE}"
-# fi
-
-ssh-keygen -t ed25519 -C "$( whoami )@$( date +%y%m%dT%H%M%S )"
