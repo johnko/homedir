34e5575aee1e78877b7fbfb5764b4a2531325e36
johnko <279736+johnko@users.noreply.github.com>
2023-08-03 17:29:33 -0400

    add healthcheck to refresh wifi

---
+++ lab/healthcheck/healthcheck-diff.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -eu
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+FILE="/var/tmp/healthcheck"
+ACTION=$1
+IP=$2
+
+if [ "ping" = "$ACTION" ]; then
+  echo "[healthcheck] starting ping..."
+  ping -c 1 -W 30 $IP \
+    && touch $FILE
+else
+  NOW=$(date "+%s")
+  THEN=$(stat $FILE --format "%Y")
+  # diff of now vs file modified time in seconds
+  DIFF=$(( NOW - THEN ))
+  # limit in seconds, eg. 600 = 10 minutes
+  LIMIT=$2
+  if [ $DIFF -gt $LIMIT ]; then
+    if [ "netplan" = "$ACTION" ]; then
+      echo "[healthcheck] starting netplan..."
+      netplan apply
+    elif [ "reboot" = "$ACTION" ]; then
+      echo "[healthcheck] starting reboot..."
+      # reboot
+    fi
+  fi
+fi
