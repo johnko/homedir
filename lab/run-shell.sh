66f295bd1d50f7f3aac289f5ffda71e054111f61
johnko <279736+johnko@users.noreply.github.com>
2023-08-04 10:06:20 -0400

    set PATH

--- lab/run-shell.sh
+++ lab/run-shell.sh
@@ -4,6 +4,12 @@ set -eux
 sed -i '' 's/127.0.0.1/labcluster/' ./tmp/k3s.yaml
 
 IMAGE_NAME="shell:local"
-docker build --platform linux/amd64 -t $IMAGE_NAME ./shell
+docker build \
+  --platform linux/amd64 \
+  -t $IMAGE_NAME ./shell
 
-docker run -it --rm --platform linux/amd64 -v ./tmp/k3s.yaml:/root/.kube/config $IMAGE_NAME
+docker run -it --rm \
+  --platform linux/amd64 \
+  -v ./tmp/k3s.yaml:/root/.kube/config \
+  --add-host labcluster:192.168.2.232 \
+  $IMAGE_NAME
