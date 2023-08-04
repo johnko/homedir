f55ef880744a15078f827de64208e88b095415d6
johnko <279736+johnko@users.noreply.github.com>
2023-08-04 07:53:12 -0400

    preload docker images

--- lab/postgres/run-postgres.sh
+++ lab/postgres/run-postgres.sh
@@ -5,6 +5,10 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 set -x
 
 case $1 in
+  cert)
+    cert_generateselfsigned ./pgsql-server
+    chown 999:999 ./pgsql-server.key
+    ;;
   destroy)
     ## Destroy the stack and data
     docker-compose down --volumes
@@ -23,19 +27,15 @@ case $1 in
     docker-compose pull
     ;;
   test-new)
-    docker-compose down --volumes
-    cert_generateselfsigned ./pgsql-server
-    chown 999:999 ./pgsql-server.key
-    docker-compose pull
-    docker-compose up --detach
+    bash $0 destroy
+    bash $0 up
     ;;
   top)
     docker-compose top
     ;;
   up | *)
-    cert_generateselfsigned ./pgsql-server
-    chown 999:999 ./pgsql-server.key
-    docker-compose pull
+    bash $0 cert
+    bash $0 pull
     ## Create and run the stack interactively
     # docker-compose up
     ## Create and run the stack in the background
