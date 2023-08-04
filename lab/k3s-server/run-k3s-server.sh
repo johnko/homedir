416b82a852198c26a6bc2b03b5838a914117f6a4
johnko <279736+johnko@users.noreply.github.com>
2023-08-04 08:32:52 -0400

    add k3s-server

---
+++ lab/k3s-server/run-k3s-server.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eu
+set +x
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -x
+
+case $1 in
+  cert)
+    cp -a /var/lib/docker/pgsql-server.pem ./pgsql-server.pem
+    ;;
+  destroy)
+    ## Destroy the stack and data
+    docker-compose down --volumes
+    ;;
+  down)
+    ## Destroy the stack but keep the data
+    docker-compose down
+    ;;
+  logs)
+    docker-compose logs
+    ;;
+  ps)
+    docker-compose ps
+    ;;
+  pull)
+    docker-compose pull
+    ;;
+  test-new)
+    bash $0 destroy
+    bash $0 up
+    ;;
+  top)
+    docker-compose top
+    ;;
+  up | *)
+    bash $0 cert
+    bash $0 pull
+    ## Create and run the stack interactively
+    # docker-compose up
+    ## Create and run the stack in the background
+    docker-compose up --detach
+    ;;
+esac
