a4e021ddaa853a6e9042b9c4d7e2495693876c21
johnko <279736+johnko@users.noreply.github.com>
2023-08-04 22:44:59 -0400

    add ucarp

--- lab/run-bootstrap.sh
+++ lab/run-bootstrap.sh
@@ -5,6 +5,10 @@ if [ "x" = "x$ANSIBLE_PASSWORD" ]; then
   echo "ERROR: Missing export ANSIBLE_PASSWORD=..."
   exit 1
 fi
+if [ "x" = "x$LABCLUSTER_IP" ]; then
+  echo "ERROR: Missing export LABCLUSTER_IP=..."
+  exit 1
+fi
 set -ux
 
 ansible-playbook -i hosts.yml playbook-bootstrap.yml
