e8731db6e4f7bfd53801ac536367eff4088ac476
johnko <279736+johnko@users.noreply.github.com>
2023-08-05 20:21:33 -0400

    handle mac

--- lab/run-k3s.sh
+++ lab/run-k3s.sh
@@ -5,6 +5,10 @@ if [ "x" = "x$ANSIBLE_PASSWORD" ]; then
   echo "ERROR: Missing export ANSIBLE_PASSWORD=..."
   exit 1
 fi
+if [ "x" = "x$ANSIBLE_PASSWORD_MAC" ]; then
+  echo "ERROR: Missing export ANSIBLE_PASSWORD_MAC=..."
+  exit 1
+fi
 set -ux
 
 mkdir -p ./tmp
