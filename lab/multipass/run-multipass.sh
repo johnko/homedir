949d22d64efdb501b8b92bea206180586b8a9024
johnko <279736+johnko@users.noreply.github.com>
2023-08-09 01:19:36 -0400

    add cloud-init to multipass

--- lab/multipass/run-multipass.sh
+++ lab/multipass/run-multipass.sh
@@ -7,9 +7,11 @@ NIC=en2
 multipass set local.driver=qemu
 
 multipass launch \
+  -vvv \
   --name vm1 \
   --cpus 4 \
   --memory 16G \
   --disk 250G \
   --network "name=$NIC,mode=auto" \
+  --cloud-init=./cloud-init.yaml \
   $IMAGE
