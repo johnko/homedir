7b9c93eacb7f5d91fd7c4c371132c52038c7efe4
johnko <279736+johnko@users.noreply.github.com>
2023-08-09 00:04:00 -0400

    switch to qemu driver

--- lab/multipass/run-multipass.sh
+++ lab/multipass/run-multipass.sh
@@ -4,12 +4,12 @@ set -eux
 IMAGE=22.04
 NIC=en2
 
-multipass set local.bridged-network=$NIC
+multipass set local.driver=qemu
 
 multipass launch \
   --name vm1 \
   --cpus 4 \
   --memory 16G \
   --disk 250G \
-  --bridged \
+  --network "name=$NIC,mode=auto" \
   $IMAGE
