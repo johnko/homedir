ff946025c78637a1f9e5c910069600d8eecccdb2
johnko <279736+johnko@users.noreply.github.com>
2023-08-11 04:12:55 -0400

    add traefik helpers

---
+++ lab/traefik/run-traefik.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eux
+
+case $1 in
+  web | *)
+    kubectl port-forward -n traefik $(kubectl get pods -n traefik --selector "app.kubernetes.io/name=traefik" --output=name) 9588:9000 &
+    open http://localhost:9588/dashboard/
+    ;;
+esac
