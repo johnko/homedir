2f7e5a5539248ccc3a6110ab84268516042957c4
johnko <279736+johnko@users.noreply.github.com>
2023-08-11 04:34:48 -0400

    pick first pod for traefik ui

--- lab/traefik/run-traefik.sh
+++ lab/traefik/run-traefik.sh
@@ -3,7 +3,7 @@ set -eux
 
 case $1 in
   web | *)
-    kubectl port-forward -n traefik $(kubectl get pods -n traefik --selector "app.kubernetes.io/name=traefik" --output=name) 9588:9000 &
+    kubectl port-forward -n traefik $(kubectl get pods -n traefik --selector "app.kubernetes.io/name=traefik" --output=name | head -n1 ) 9588:9000 &
     open http://localhost:9588/dashboard/
     ;;
 esac
