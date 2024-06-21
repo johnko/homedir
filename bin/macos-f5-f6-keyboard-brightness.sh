0872a97c71903ae08e75e4ee81dc6ca84bfc3c5b
johnko <279736+johnko@users.noreply.github.com>
2024-06-20 22:11:07 -0400

    macos-f5 update comment

--- bin/macos-f5-f6-keyboard-brightness.sh
+++ bin/macos-f5-f6-keyboard-brightness.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -eux
 
+# replaces (dictation button) F5 and (focus button) F6 with keyboard brightness and eject to play/pause
+
 # source: https://www.reddit.com/r/macbook/comments/x3ovje/is_there_a_way_to_adjust_keyboard_brightness_from/
 # other: https://hidutil-generator.netlify.app/
 # other: https://mybyways.com/blog/script-to-re-map-macbook-pro-function-keys
 
-# replaces (dictation button) F5 and (focus button) F6 with keyboard brightness and eject to play/pause
+# fn_keys=(0 0x70000003a 0x70000003b 0x70000003c 0x70000003d 0x70000003e 0x70000003f 0x700000040 0x700000041 0x700000042 0x700000043 0x700000044 0x700000045 0x700000068 0x700000069 0x70000006a 0x70000006b 0x70000006c 0x70000006d 0x70000006e)
 
 # fn_dict=0xc000000cf
 # f5=0x70000003E
@@ -23,8 +25,10 @@ MAPPING='
   "UserKeyMapping":[
     {"HIDKeyboardModifierMappingSrc": 0xC000000CF,"HIDKeyboardModifierMappingDst": 0xFF00000009},
     {"HIDKeyboardModifierMappingSrc": 0x70000003E,"HIDKeyboardModifierMappingDst": 0xFF00000009},
+
     {"HIDKeyboardModifierMappingSrc": 0x10000009B,"HIDKeyboardModifierMappingDst": 0xFF00000008},
     {"HIDKeyboardModifierMappingSrc": 0x70000003F,"HIDKeyboardModifierMappingDst": 0xFF00000008},
+
     {"HIDKeyboardModifierMappingSrc": 0xC000000B8,"HIDKeyboardModifierMappingDst": 0xC000000CD}
   ]
 }'
