3d559bda02a1a6eb19a6f2e6a82d23aab59bcccf
johnko <279736+johnko@users.noreply.github.com>
2024-06-20 21:56:51 -0400

    add key hex refs

--- bin/macos-f5-f6-keyboard-brightness.sh
+++ bin/macos-f5-f6-keyboard-brightness.sh
@@ -3,8 +3,21 @@ set -eux
 
 # source: https://www.reddit.com/r/macbook/comments/x3ovje/is_there_a_way_to_adjust_keyboard_brightness_from/
 # other: https://hidutil-generator.netlify.app/
+# other: https://mybyways.com/blog/script-to-re-map-macbook-pro-function-keys
 
 # replaces (dictation button) F5 and (focus button) F6 with keyboard brightness and eject to play/pause
+
+# fn_dict=0xc000000cf
+# f5=0x70000003E
+# fn_kbd_dec=0xff00000009
+
+# fn_dnd=0x10000009b
+# f6=0x70000003F
+# fn_kbd_inc=0xff00000008
+
+# eject=0xC000000B8
+# fn_play=0xc000000cd
+
 MAPPING='
 {
   "UserKeyMapping":[
