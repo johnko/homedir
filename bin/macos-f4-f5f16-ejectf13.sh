919b95778fd9cc7b6913249d95d6aace57418df6
johnko <279736+johnko@users.noreply.github.com>
2024-10-22 22:30:54 -0400

    change to macos-f4-f5f16-ejectf13.sh

---
+++ bin/macos-f4-f5f16-ejectf13.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -eux
+
+# replaces (spotlight/apps button) F4 to F4,
+# (dictation button) F5 to F16,
+# and eject to F13
+
+# source: https://www.reddit.com/r/macbook/comments/x3ovje/is_there_a_way_to_adjust_keyboard_brightness_from/
+# other: https://hidutil-generator.netlify.app/
+# other: https://mybyways.com/blog/script-to-re-map-macbook-pro-function-keys
+
+# fn_keys=(0 0x70000003a 0x70000003b 0x70000003c 0x70000003d 0x70000003e 0x70000003f 0x700000040 0x700000041 0x700000042 0x700000043 0x700000044 0x700000045 0x700000068 0x700000069 0x70000006a 0x70000006b 0x70000006c 0x70000006d 0x70000006e)
+
+# fn_spotl=0xff0100000001   # or 0xc00000221
+# fn_launchp=0xff0100000004
+# f4=0x70000003D
+
+# fn_dict=0xc000000cf
+# f5=0x70000003E
+# f16=0x70000006b
+
+# eject=0xC000000B8
+# f13=0x700000068
+
+MAPPING='
+{
+  "UserKeyMapping":[
+    {"HIDKeyboardModifierMappingSrc": 0xFF0100000001,"HIDKeyboardModifierMappingDst": 0x70000003D},
+    {"HIDKeyboardModifierMappingSrc": 0xC00000221,"HIDKeyboardModifierMappingDst": 0x70000003D},
+    {"HIDKeyboardModifierMappingSrc": 0xFF0100000004,"HIDKeyboardModifierMappingDst": 0x70000003D},
+    {"HIDKeyboardModifierMappingSrc": 0x70000003D,"HIDKeyboardModifierMappingDst": 0x70000003D},
+
+    {"HIDKeyboardModifierMappingSrc": 0xC000000CF,"HIDKeyboardModifierMappingDst": 0x70000006b},
+    {"HIDKeyboardModifierMappingSrc": 0x70000003E,"HIDKeyboardModifierMappingDst": 0x70000006b},
+
+    {"HIDKeyboardModifierMappingSrc": 0xC000000B8,"HIDKeyboardModifierMappingDst": 0x700000068}
+  ]
+}'
+
+/usr/bin/hidutil property --set "$(echo $MAPPING | tr -d ' ')"
