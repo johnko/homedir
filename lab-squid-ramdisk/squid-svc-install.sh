67a07deca49ab2b3ee8bff5169f73709b4782cc8
johnko <279736+johnko@users.noreply.github.com>
2025-01-11 02:45:36 -0500

    correct start script name

--- lab-squid-ramdisk/squid-svc-install.sh
+++ lab-squid-ramdisk/squid-svc-install.sh
@@ -3,7 +3,7 @@
 set -e
 
 install -d -m 755 -o root /usr/local/bin
-install -m 755 -o root start.sh /usr/local/bin/start.sh
+install -m 755 -o root squid-svc-start.sh /usr/local/bin/squid-svc-start.sh
 
 install -m 644 -o root openssl.cnf.add /etc/ssl/openssl.cnf.add
 cat /etc/ssl/openssl.cnf.add >> /etc/ssl/openssl.cnf
