f64bf62616a1a508951bc6412da5dae967864ca1
johnko <279736+johnko@users.noreply.github.com>
2025-01-10 22:54:24 -0500

    add lab-squid-ramdisk

---
+++ lab-squid-ramdisk/run-vm2.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -eu
+set +x
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+set -x
+
+IMAGE=24.04
+NIC=en2
+NAME=vm2
+
+
+case $1 in
+  destroy)
+    set +e
+    while multipass list | grep vm1 ; do
+      sleep 1
+      multipass stop $NAME
+      multipass delete $NAME
+      multipass purge
+    done
+    ;;
+  info)
+    multipass info $NAME
+    ;;
+  ps)
+    ps aux | grep qemu
+    ;;
+  restart)
+    launchctl unload /Library/LaunchDaemons/com.canonical.multipassd.plist
+    launchctl load -w /Library/LaunchDaemons/com.canonical.multipassd.plist
+    ;;
+  stop)
+    multipass stop $NAME
+    ;;
+  start | *)
+    ls -l /Volumes/RAMDisk
+    if test -h /var/root/Library/Application\ Support/multipassd ; then
+      ls -l /var/root/Library/Application\ Support/multipassd
+    elif test -d /var/root/Library/Application\ Support/multipassd ; then
+      launchctl unload /Library/LaunchDaemons/com.canonical.multipassd.plist
+      install -d -v -g wheel -m 755 -o root /Volumes/RAMDisk/vm
+      mv /var/root/Library/Application\ Support/multipassd /Volumes/RAMDisk/vm/multipassd
+      ln -s /Volumes/RAMDisk/vm/multipassd /var/root/Library/Application\ Support/multipassd
+      launchctl load -w /Library/LaunchDaemons/com.canonical.multipassd.plist
+    else
+      launchctl unload /Library/LaunchDaemons/com.canonical.multipassd.plist
+      install -d -v -g wheel -m 755 -o root /Volumes/RAMDisk/vm/multipassd
+      ln -s /Volumes/RAMDisk/vm/multipassd /var/root/Library/Application\ Support/multipassd
+      launchctl load -w /Library/LaunchDaemons/com.canonical.multipassd.plist
+    fi
+    multipass get local.driver | grep qemu || multipass set local.driver=qemu
+    multipass start $NAME \
+    || multipass launch \
+      -vvv \
+      --name=$NAME \
+      --cpus=4 \
+      --memory=8G \
+      --disk=20G \
+      --network="name=$NIC,mode=auto" \
+      --cloud-init=./cloud-init.yaml \
+      $IMAGE
+    ;;
+esac
