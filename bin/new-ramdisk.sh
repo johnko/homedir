fe3cf09f9d0b571f462a52bd68094bee9925439b
johnko <279736+johnko@users.noreply.github.com>
2025-01-10 22:09:37 -0500

    use apfs

--- bin/new-ramdisk.sh
+++ bin/new-ramdisk.sh
@@ -3,4 +3,5 @@ set -eux
 
 # 1 GB = 2097152
 # 2 GB = 4194304
-diskutil erasevolume HFS+ "RAMDisk" `hdiutil attach -nomount ram://$(($1 * 2097152))`
+diskutil apfs create $( hdiutil attach -nomount ram://$(($1 * 2097152)) ) RAMDisk && \
+  touch /Volumes/RAMDisk/.metadata_never_index
