1353bd6f40888c0d9e3623b2642883381e31cea0
johnko <279736+johnko@users.noreply.github.com>
2025-01-22 18:37:40 -0500

    try force if fail

--- bin/macos-brew-update.sh
+++ bin/macos-brew-update.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 if which brew >/dev/null 2>&1 ; then
   if [[ -e Brewfile ]] ; then
     set -x
-    brew bundle
-    set +x
-    cat Brewfile | grep cask | grep -v '^#' | cut -d' ' -f2 | tr -d '"' | xargs -t -I{} brew install --cask {}
+    brew bundle || true
+    for i in $(cat Brewfile | grep cask | grep -v '^#' | cut -d' ' -f2 | tr -d '"') ; do
+      set -x
+      brew install --cask $i || brew install --force --cask $i
+      set +x
+    done
   else
     echo "ERROR: missing 'Brewfile'"
     exit 1
