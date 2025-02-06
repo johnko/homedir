ec9e45e4a7ecbb3baa4ff5eddef7ead32d6e091c
johnko <279736+johnko@users.noreply.github.com>
2025-02-06 00:57:14 -0500

    run ollama only on localhost by default

--- bin/macos-ollama-server.sh
+++ bin/macos-ollama-server.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export OLLAMA_HOST=`get-ip`:11434
+export OLLAMA_HOST=127.0.0.1:11434
+# export OLLAMA_HOST=`get-ip`:11434
+# export OLLAMA_HOST=0.0.0.0:11434
 
 if type ollama &>/dev/null ; then
   ollama serve
