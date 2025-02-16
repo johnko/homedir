659b002ff4da37de8b1461f30ca0ab813dc2fe1c
johnko <279736+johnko@users.noreply.github.com>
2025-02-16 16:58:40 -0500

    re-order OLLAMA_HOST

--- bin/macos-ollama-server.sh
+++ bin/macos-ollama-server.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# last one wins
+export OLLAMA_HOST=0.0.0.0:11434
+export OLLAMA_HOST=`get-ip`:11434
 export OLLAMA_HOST=127.0.0.1:11434
-# export OLLAMA_HOST=`get-ip`:11434
-# export OLLAMA_HOST=0.0.0.0:11434
 
 if type ollama &>/dev/null ; then
   ollama serve
