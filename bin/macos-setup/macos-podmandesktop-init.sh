e83382ab091851da75be729f216b35ff2c7ad665
johnko <279736+johnko@users.noreply.github.com>
2025-02-28 22:29:52 -0500

    podman machine with 8GB RAM

--- bin/macos-setup/macos-podmandesktop-init.sh
+++ bin/macos-setup/macos-podmandesktop-init.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if type podman &>/dev/null ; then
   set -x
-  podman machine init
+  podman machine init --memory $((1024 * 8))
   podman machine start
   # podman machine set --rootful
 else
