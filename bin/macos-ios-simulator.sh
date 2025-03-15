053976d4eaf9645e787f5a635b36c45399207ae7
johnko <279736+johnko@users.noreply.github.com>
2025-03-14 23:51:35 -0400

    open sim

--- bin/macos-ios-simulator.sh
+++ bin/macos-ios-simulator.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-set -x
-
 DEVICE_NAME='my-iphone16'
 DEVICE_TYPE='iPhone 16 Pro'
 EXCLUDE_DEVICE_TYPE='Max'
 
 if type xcrun &>/dev/null ; then
-  SIM_DEVICE_TYPE=$(xcrun simctl list devicetypes | grep $DEVICE_TYPE | grep -v $EXCLUDE_DEVICE_TYPE | grep -o '(.*)' | tr -d '()')
+  set +u
+  ID=$(xcrun simctl list devices | grep "$DEVICE_NAME" | awk '{print $2}' | tr -d '()')
+  if [ -z "$ID" ] ; then
+    set -u
+    SIM_DEVICE_TYPE=$(xcrun simctl list devicetypes | grep "$DEVICE_TYPE" | grep -v "$EXCLUDE_DEVICE_TYPE" | grep -o '(.*)' | tr -d '()')
 
-  SIM_RUNTIME_ID=$(xcrun simctl list runtimes | grep -o ' - [^ ]*$' | sed 's, - ,,')
+    SIM_RUNTIME_ID=$(xcrun simctl list runtimes | grep -o ' - [^ ]*$' | sed 's, - ,,')
 
-  xcrun simctl create $DEVICE_NAME $SIM_DEVICE_TYPE $SIM_RUNTIME_ID
+    xcrun simctl create $DEVICE_NAME $SIM_DEVICE_TYPE $SIM_RUNTIME_ID
+
+    ID=$(xcrun simctl list devices | grep "$DEVICE_NAME")
+  fi
+
+  echo "My devices:"
+  echo $ID
+
+  xcrun simctl shutdown $ID || true
+
+  xcrun simctl erase $ID
+
+  xcrun simctl boot $ID
+
+  open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app
 else
   echo "ERROR: missing 'xcrun'"
   exit 1
