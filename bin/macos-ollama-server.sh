a8bf38284861770bad9c351958ac97c163141044
johnko <279736+johnko@users.noreply.github.com>
2025-04-03 22:47:44 -0400

    optional arg for bin/macos-ollama-server.sh

--- bin/macos-ollama-server.sh
+++ bin/macos-ollama-server.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
 # last one wins
-export OLLAMA_HOST=0.0.0.0:11434
-export OLLAMA_HOST=`get-ip`:11434
-export OLLAMA_HOST=127.0.0.1:11434
+case $1 in
+  public)
+    export OLLAMA_HOST=0.0.0.0:11434
+    ;;
+  lan)
+    export OLLAMA_HOST=`get-ip | head -n1`:11434
+    ;;
+  *)
+    export OLLAMA_HOST=127.0.0.1:11434
+    ;;
+esac
+echo "OLLAMA_HOST=$OLLAMA_HOST"
+
+set -u
 
 if type ollama &>/dev/null ; then
   ollama serve
