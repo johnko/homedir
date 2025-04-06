1e56f45d0dc91b5e0467542928b23ef61d9f39e9
johnko <279736+johnko@users.noreply.github.com>
2025-04-05 22:27:49 -0400

    instructions

--- bin/macos-setup/macos-airplay-off.sh
+++ bin/macos-setup/macos-airplay-off.sh
@@ -6,3 +6,7 @@ set -x
 defaults -currentHost write com.apple.controlcenter.plist AirplayReceiverEnabled -bool false
 
 defaults -currentHost read  com.apple.controlcenter.plist AirplayReceiverEnabled
+
+set +x
+
+echo "You need to reboot for the setting to take effect."
