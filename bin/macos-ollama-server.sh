a090b9d3af7d32ff8727e0c52821053e700325d2
johnko <279736+johnko@users.noreply.github.com>
2025-04-07 23:30:12 -0400

    relaunch if crash

--- bin/macos-ollama-server.sh
+++ bin/macos-ollama-server.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+export CADDY_OLLAMA_API_DOMAIN=$(op read op://Private/1pcli-caddyollama-api-domain/host)
+export CADDY_OLLAMA_API_USER1=$(op read op://Private/1pcli-caddyollama-api-user1/username)
+export CADDY_OLLAMA_API_TEMP_PASSWORD1=$(op read op://Private/1pcli-caddyollama-api-user1/password)
+export CADDY_OLLAMA_API_PUBKEY1=$(op read "op://Private/1pcli-caddyollama-api-pubkey1/public key")
+export CADDY_OLLAMA_API_PUBKEY2=$(op read "op://Private/1pcli-caddyollama-api-pubkey2/public key")
+
 if type docker-compose &>/dev/null ; then
   DOCKERCOMPOSE_BIN="docker-compose"
 else
@@ -16,68 +22,70 @@ else
   fi
 fi
 
-case $1 in
-  logs)
-    $DOCKER_BIN logs -f caddy 2>&1 | sed "s,^{,\n{,"
-    ;;
-  public)
-    export OLLAMA_HOST=`get-ip | tail -n1`:11434
-    set -u
+relaunch_ollama() {
+  case $1 in
+    logs)
+      $DOCKER_BIN logs -f caddy 2>&1 | sed "s,^{,\n{,"
+      ;;
+    public)
+      set +e
+      set -u
+      export OLLAMA_HOST=`get-ip | tail -n1`:11434
 
-    # run caddy container so we can have some kind of authentication
-    export CADDY_COMPOSE_FOLDER="$(dirname $0)/../ollama-caddy"
-    if [ -d "$CADDY_COMPOSE_FOLDER" ]; then
-      pushd "$CADDY_COMPOSE_FOLDER"
-        export CADDY_OLLAMA_API_DOMAIN=$(op read op://Private/1pcli-caddyollama-api-domain/host)
-        export CADDY_OLLAMA_API_USER1=$(op read op://Private/1pcli-caddyollama-api-user1/username)
-        export CADDY_OLLAMA_API_TEMP_PASSWORD1=$(op read op://Private/1pcli-caddyollama-api-user1/password)
-        export CADDY_OLLAMA_API_PUBKEY1=$(op read "op://Private/1pcli-caddyollama-api-pubkey1/public key")
-        export CADDY_OLLAMA_API_PUBKEY2=$(op read "op://Private/1pcli-caddyollama-api-pubkey2/public key")
+      # run caddy container so we can have some kind of authentication
+      export CADDY_COMPOSE_FOLDER="$(dirname $0)/../ollama-caddy"
+      if [ -d "$CADDY_COMPOSE_FOLDER" ]; then
+        pushd "$CADDY_COMPOSE_FOLDER"
+          # export CLOUDFLARE_IPV4=$(curl 'https://www.cloudflare.com/ips-v4/#' | tr "\n" ' ')
+          # export CLOUDFLARE_IPV6=$(curl 'https://www.cloudflare.com/ips-v6/#' | tr "\n" ' ')
+          # export CLOUDFLARE_IP_RANGES="127.0.0.1 192.168.2.0/24 $CLOUDFLARE_IPV4 $CLOUDFLARE_IPV6"
 
-        # export CLOUDFLARE_IPV4=$(curl 'https://www.cloudflare.com/ips-v4/#' | tr "\n" ' ')
-        # export CLOUDFLARE_IPV6=$(curl 'https://www.cloudflare.com/ips-v6/#' | tr "\n" ' ')
-        # export CLOUDFLARE_IP_RANGES="127.0.0.1 192.168.2.0/24 $CLOUDFLARE_IPV4 $CLOUDFLARE_IPV6"
+          # build temp container image
+          touch Caddyfile
+          $DOCKERCOMPOSE_BIN build caddy
 
-        # build temp container image
-        touch Caddyfile
-        $DOCKERCOMPOSE_BIN build caddy
+          export CADDY_OLLAMA_API_PASSWORD1=$((echo $CADDY_OLLAMA_API_TEMP_PASSWORD1 ; echo $CADDY_OLLAMA_API_TEMP_PASSWORD1 ) | $DOCKER_BIN run --rm -it --entrypoint caddy caddy:local hash-password | tail -n 1)
 
-        export CADDY_OLLAMA_API_PASSWORD1=$((echo $CADDY_OLLAMA_API_TEMP_PASSWORD1 ; echo $CADDY_OLLAMA_API_TEMP_PASSWORD1 ) | $DOCKER_BIN run --rm -it --entrypoint caddy caddy:local hash-password | tail -n 1)
+          if [ -z "$CADDY_OLLAMA_API_PASSWORD1" ]; then
+            exit 1
+          fi
 
-        if [ -z "$CADDY_OLLAMA_API_PASSWORD1" ]; then
-          exit 1
-        fi
+          # replace env vars and generate config files
+          envsubst < ./Caddyfile.template > ./Caddyfile
+          envsubst < ./ssh/authorized_keys.template > ./ssh/authorized_keys
 
-        # replace env vars and generate config files
-        envsubst < ./Caddyfile.template > ./Caddyfile
-        envsubst < ./ssh/authorized_keys.template > ./ssh/authorized_keys
+          # rebuild final container image
+          $DOCKERCOMPOSE_BIN build caddy
+          $DOCKERCOMPOSE_BIN build ssh
+          # run container
+          $DOCKERCOMPOSE_BIN down
+          $DOCKERCOMPOSE_BIN up -d
+        popd
+      else
+        echo "ERROR: missing folder $CADDY_COMPOSE_FOLDER"
+        exit 1
+      fi
+      ;;
+    lan)
+      export OLLAMA_HOST=`get-ip | tail -n1`:11434
+      set -u
+      ;;
+    *)
+      export OLLAMA_HOST=127.0.0.1:11434
+      set -u
+      ;;
+  esac
+  echo "OLLAMA_HOST=$OLLAMA_HOST"
 
-        # rebuild final container image
-        $DOCKERCOMPOSE_BIN build caddy
-        $DOCKERCOMPOSE_BIN build ssh
-        # run container
-        $DOCKERCOMPOSE_BIN down
-        $DOCKERCOMPOSE_BIN up -d
-      popd
-    else
-      echo "ERROR: missing folder $CADDY_COMPOSE_FOLDER"
-      exit 1
-    fi
-    ;;
-  lan)
-    export OLLAMA_HOST=`get-ip | tail -n1`:11434
-    set -u
-    ;;
-  *)
-    export OLLAMA_HOST=127.0.0.1:11434
-    set -u
-    ;;
-esac
-echo "OLLAMA_HOST=$OLLAMA_HOST"
+  if type ollama &>/dev/null ; then
+    ollama serve
+  else
+    echo "ERROR: missing 'ollama'"
+    exit 1
+  fi
+}
 
-if type ollama &>/dev/null ; then
-  ollama serve
-else
-  echo "ERROR: missing 'ollama'"
-  exit 1
-fi
+while true ; do
+  set +u
+  relaunch_ollama $1
+done
