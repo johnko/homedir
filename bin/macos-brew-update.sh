441a1fa92c7c46e3dfe298d8d2375cd41e7822dd
johnko <279736+johnko@users.noreply.github.com>
2025-04-30 01:35:15 -0400

    handle some errors

--- bin/macos-brew-update.sh
+++ bin/macos-brew-update.sh
@@ -7,7 +7,7 @@ if type brew &>/dev/null ; then
     brew bundle || true
     for i in $(cat Brewfile | grep cask | grep -v '^#' | cut -d' ' -f2 | tr -d '"') ; do
       set -x
-      brew install --cask $i || brew install --force --cask $i
+      brew install --cask $i || brew install --force --cask $i || true
       set +x
     done
     brew cleanup
