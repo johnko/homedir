c22f910e306f6b36a3b6dc9e480cbf4a366f5660
johnko <279736+johnko@users.noreply.github.com>
2025-08-23 19:44:23 -0400

    allow flip sudo NOPASSWD on/off

--- bin/macos-setup/macos-sudo.sh
+++ bin/macos-setup/macos-sudo.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eo pipefail
 
+case $1 in
+  on|ON|enable|ENABLE|yes|YES)
+    export ANSIBLE_ACTION=present
+    ;;
+  *)
+    export ANSIBLE_ACTION=absent
+    ;;
+esac
+
+set -u
 echo "=>  Installing suduoer.d..."
 
 pushd $(dirname $0)
 
 set -x
 
-ansible-playbook -i localhost, ./playbook-bootstrap.yml
+echo "ANSIBLE_ACTION=$ANSIBLE_ACTION"
+ansible-playbook --ask-become-pass -i localhost, ./playbook-sudo-nopasswd.yml
 
 set +x
 
