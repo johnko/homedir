05bc3a2b34ff19f2ca12b01b44df74e5d6a66d96
johnko <279736+johnko@users.noreply.github.com>
2025-08-24 00:21:33 -0400

    add terminal-toggle-sudo.scpt

---
+++ .osascripts/terminal-toggle-sudo.scpt
@@ -0,0 +1,4 @@
+tell application "Terminal"
+	set currentTab to do script ("export ARG=off ; grep NOPASSWD /etc/sudoers.d/50-local-user || export ARG=on ; bash ~/bin/macos-setup/macos-sudo.sh $ARG")
+	activate application "Terminal"
+end tell
--- bin/macos-setup/macos-sudo.sh
+++ bin/macos-setup/macos-sudo.sh
@@ -17,11 +17,11 @@ echo "=>  - ANSIBLE_ACTION=$ANSIBLE_ACTION"
 
 pushd $(dirname $0)
 
+set -x
 
 export ANSIBLE_BECOME="false"
-if ! ansible-playbook -i localhost, ./playbook-sudo-nopasswd.yml ; then
+if ansible-playbook --check --diff -i localhost, ./playbook-sudo-nopasswd.yml ; then
   export ANSIBLE_BECOME="true"
-  set -x
   ansible-playbook --ask-become-pass -i localhost, ./playbook-sudo-nopasswd.yml
 fi
 
