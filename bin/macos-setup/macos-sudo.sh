2704445d97c6649284ed0d12be519d6e95008f5d
johnko <279736+johnko@users.noreply.github.com>
2025-08-24 00:09:09 -0400

    fixup

--- bin/macos-setup/macos-sudo.sh
+++ bin/macos-setup/macos-sudo.sh
@@ -11,16 +11,17 @@ case $1 in
 esac
 
 set -u
-echo "=>  Installing suduoer.d..."
+
+echo "=>  Installing sudoers.d..."
+echo "=>  - ANSIBLE_ACTION=$ANSIBLE_ACTION"
 
 pushd $(dirname $0)
 
-set -x
 
-echo "ANSIBLE_ACTION=$ANSIBLE_ACTION"
 export ANSIBLE_BECOME="false"
 if ! ansible-playbook -i localhost, ./playbook-sudo-nopasswd.yml ; then
   export ANSIBLE_BECOME="true"
+  set -x
   ansible-playbook --ask-become-pass -i localhost, ./playbook-sudo-nopasswd.yml
 fi
 
