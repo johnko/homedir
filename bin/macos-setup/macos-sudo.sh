346e566edfcf67539f78273e38353c33f4e94fd2
johnko <279736+johnko@users.noreply.github.com>
2025-09-02 19:09:49 -0400

    remove if

--- bin/macos-setup/macos-sudo.sh
+++ bin/macos-setup/macos-sudo.sh
@@ -17,13 +17,16 @@ echo "=>  - ANSIBLE_ACTION=$ANSIBLE_ACTION"
 
 pushd $(dirname $0)
 
+set +e
 set -x
 
 export ANSIBLE_BECOME="false"
-if ansible-playbook --check --diff -i localhost, ./playbook-sudo-nopasswd.yml ; then
-  export ANSIBLE_BECOME="true"
-  ansible-playbook --ask-become-pass -i localhost, ./playbook-sudo-nopasswd.yml
-fi
+ansible-playbook --check --diff -i localhost, ./playbook-sudo-nopasswd.yml
+
+set -e
+
+export ANSIBLE_BECOME="true"
+ansible-playbook --ask-become-pass -i localhost, ./playbook-sudo-nopasswd.yml
 
 set +x
 
