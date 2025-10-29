1e0977a48d8b6ac818cb39e7de8f36b1dc78b622
johnko <279736+johnko@users.noreply.github.com>
2025-10-29 14:49:37 -0400

    prune vscode extensions too

--- bin/macos-brew-prune.sh
+++ bin/macos-brew-prune.sh
@@ -7,6 +7,19 @@ if type brew &>/dev/null ; then
     if [[ -e .Brewfile ]] ; then
       EXTRA_BREWFILE=".Brewfile"
     fi
+    if type code &>/dev/null ; then
+      echo "Pruning VSCode extensions"
+      for i in $(
+        code --list-extensions | \
+        grep -v -E "docker|terraform|"$(
+          cat Brewfile $EXTRA_BREWFILE | grep -v 'instance_eval' | grep -v '^#' | grep -v '^$' | tr -d , | tr -d '"' | awk '{print $2}' | tr "\n" '|' | sed 's,|$,,'
+        )
+      ); do
+        code --uninstall-extension $i &>/dev/null && (echo ; echo "code --uninstall-extension $i") || echo -n "."
+      done
+    fi
+    echo
+    echo "Pruning Homebrew packages"
     for i in $(
       brew list | \
       grep -v -E "1password|applesimutils|docker|terraform|"$(
