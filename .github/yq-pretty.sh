9868659437a0277e3e76897fa17d3393c086a4bd
John Ko <279736+johnko@users.noreply.github.com>
2025-11-08 00:59:35 -0500

    Add .github/yq-pretty.sh

--- .github/yq-pretty.sh
+++ .github/yq-pretty.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 
 yq --version
 
-find . -type f \( -name '*.yaml' -o -name '*.yml' \) -print0 | xargs -0 -P2 -I{} yq --exit-status --inplace --no-colors --prettyPrint 'sort_keys(..)' {}
+find . -type f \( -name '*.yaml' -o -name '*.yml' \) -not -path '*/lab/charts/*' -not -path '*/lab/traefik/*' -not -path '*/docker-files/squid/ref/*' -print0 | xargs -0 --max-procs=2 --verbose -I{} yq --exit-status --inplace --no-colors --prettyPrint 'sort_keys(..)' {}
 
 bash -ex ./.github/git-has-uncommited-changes.sh
