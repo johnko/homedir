535aae048ece30d968e567b7bae6be6406b5ca6c
John Ko <279736+johnko@users.noreply.github.com>
2025-11-16 12:04:30 -0500

    Add .github/terraform-validate.sh

--- .github/terraform-validate.sh
+++ .github/terraform-validate.sh
@@ -31,6 +31,6 @@ export IAC_BIN=terraform
 
 $IAC_BIN version
 
-for WORKSPACE in $(find . -name '*.tf' -print0 | xargs -0 -I{} dirname {} | sort -u); do
+for WORKSPACE in $(find . -name '*.tf' -not -path '*/docs/*/example/*' -print0 | xargs -0 -I{} dirname {} | sort -u); do
   bash -e .github/tf.sh "$WORKSPACE" validate
 done
