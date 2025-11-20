c6846459d109deed5f5e93c1ea18e3c6a4c77c0a
johnko <279736+johnko@users.noreply.github.com>
2025-11-20 00:15:46 -0500

    podman rootful for dockerprom

--- bin/macos-setup/macos-podmandesktop-init.sh
+++ bin/macos-setup/macos-podmandesktop-init.sh
@@ -19,8 +19,8 @@ if type podman &>/dev/null; then
   fi
   set -x
   podman machine init --cpus 8 --disk-size 150 --memory $((1024 * MAX_MEMORY))
+  podman machine set --rootful
   podman machine start
-  # podman machine set --rootful
 else
   echo "ERROR: missing 'podman'"
   exit 1
