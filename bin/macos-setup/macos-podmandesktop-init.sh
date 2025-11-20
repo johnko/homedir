fe5360cdff1bc2609b312863320974449f91ceb2
johnko <279736+johnko@users.noreply.github.com>
2025-11-19 19:41:49 -0500

    chore: increase podman resources

--- bin/macos-setup/macos-podmandesktop-init.sh
+++ bin/macos-setup/macos-podmandesktop-init.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 if type podman &>/dev/null; then
   set -x
-  podman machine init --memory $((1024 * 8))
+  podman machine init --cpus 8 --disk-size 150 --memory $((1024 * 8))
   podman machine start
   # podman machine set --rootful
 else
