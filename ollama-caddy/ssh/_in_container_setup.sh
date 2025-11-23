3a00c09fc9057565fdb15601dee9c618f8d808b5
johnko <279736+johnko@users.noreply.github.com>
2025-11-23 00:43:00 -0500

    comment

--- ollama-caddy/ssh/_in_container_setup.sh
+++ ollama-caddy/ssh/_in_container_setup.sh
@@ -17,6 +17,7 @@ chmod 400 /root/.ssh/authorized_keys
 NEW_USERNAME=jumpbox
 
 useradd --create-home --shell /bin/bash --home-dir /home/$NEW_USERNAME $NEW_USERNAME
+# unlock new user but invalid password
 usermod -p '*' $NEW_USERNAME
 install -d -m 500 -o $NEW_USERNAME -g $NEW_USERNAME /home/$NEW_USERNAME/.ssh
 install -m 400 -o $NEW_USERNAME -g $NEW_USERNAME /root/.ssh/authorized_keys /home/$NEW_USERNAME/.ssh/authorized_keys
