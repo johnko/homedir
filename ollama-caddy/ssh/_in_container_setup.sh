a0b7d98ba1f57a2ce043c4459b32fd3c376ffa77
johnko <279736+johnko@users.noreply.github.com>
2025-11-23 00:39:49 -0500

    non-root ssh

---
+++ ollama-caddy/ssh/_in_container_setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+apt update -y
+
+apt install -y \
+  locales \
+  openssh-server
+
+locale-gen en_US.UTF-8
+
+mkdir /run/sshd
+
+install -d -m 500 /root/.ssh
+chmod 400 /root/.ssh/authorized_keys
+
+NEW_USERNAME=jumpbox
+
+useradd --create-home --shell /bin/bash --home-dir /home/$NEW_USERNAME $NEW_USERNAME
+usermod -p '*' $NEW_USERNAME
+install -d -m 500 -o $NEW_USERNAME -g $NEW_USERNAME /home/$NEW_USERNAME/.ssh
+install -m 400 -o $NEW_USERNAME -g $NEW_USERNAME /root/.ssh/authorized_keys /home/$NEW_USERNAME/.ssh/authorized_keys
