7d64654d019d6bb3750290563c2fe3591ee11141
johnko <279736+johnko@users.noreply.github.com>
2026-01-30 00:38:25 -0500

    abort in macos

--- bin/opencode.sh
+++ bin/opencode.sh
@@ -4,6 +4,11 @@ set -euo pipefail
 # renovate: datasource=github-releases depName=nodejs/node packageName=nodejs/node
 NODE_VERSION=24.12.0
 
+if uname | grep -i "darwin"; then
+  echo "ERROR: detected running in macOS. Aborting."
+  exit 1
+fi
+
 if type mise &>/dev/null; then
   ## run TUI/CLI
   mise exec node@$NODE_VERSION -- opencode "$@"
