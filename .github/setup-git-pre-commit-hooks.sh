ee77dd01c734b3cb89c0aaa377c5efee6639cd69
johnko <279736+johnko@users.noreply.github.com>
2026-02-08 06:06:13 -0500

    reset

--- .github/setup-git-pre-commit-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euxo pipefail
-
-SCRIPT_DIR=$(dirname "$0")
-pushd "$SCRIPT_DIR"
-
-if [[ -e ./pre-commit.sh ]] && [[ -d ../.git/hooks ]]; then
-  install -m 700 ./pre-commit.sh ../.git/hooks/pre-commit
-fi
