9c0ce38a3a187f86585933476771e280e9eed293
johnko <279736+johnko@users.noreply.github.com>
2026-02-17 04:08:06 -0500

    don't restart yabai, just switch layout and reapply rules

--- bin/macos-restart-hammerspoon-yabai.sh
+++ bin/macos-restart-hammerspoon-yabai.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-set +u
-YABAI_WINDOW_ID="$1"
-if [[ -z "$YABAI_WINDOW_ID" ]]; then
-  # only stop yabai if called without window id
-  yabai --stop-service || true
-fi
+# continue on failure
+set +e
 
+yabai -m config layout float
 open -g hammerspoon://autolayout
-
-if [[ -z "$YABAI_WINDOW_ID" ]]; then
-  # only start yabai if called without window id because it was stopped
-  yabai --start-service
-else
-  # called from yabai signal so restart it
-  yabai --restart-service
-fi
+yabai -m rule --apply
+yabai -m config layout bsp
