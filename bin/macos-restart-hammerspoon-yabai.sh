76d410b942c74cc9463edaa2349eecd815753c33
johnko <279736+johnko@users.noreply.github.com>
2026-02-17 03:51:16 -0500

    better restart on new window

--- bin/macos-restart-hammerspoon-yabai.sh
+++ bin/macos-restart-hammerspoon-yabai.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-yabai --stop-service ; open -g hammerspoon://autolayout ; yabai --start-service
+set +u
+YABAI_WINDOW_ID="$1"
+if [[ -z "$YABAI_WINDOW_ID" ]]; then
+  # only stop yabai if called without window id
+  yabai --stop-service || true
+fi
+
+open -g hammerspoon://autolayout
+
+if [[ -z "$YABAI_WINDOW_ID" ]]; then
+  # only start yabai if called without window id because it was stopped
+  yabai --start-service
+else
+  # called from yabai signal so restart it
+  yabai --restart-service
+fi
