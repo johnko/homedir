388c47c06a35f09166ac151adad8546065ca7b00
johnko <279736+johnko@users.noreply.github.com>
2026-02-18 21:00:44 -0500

    usage

--- .hammerspoon/windowhotkeys.lua
+++ .hammerspoon/windowhotkeys.lua
@@ -1,7 +1,24 @@
 -- Hotkeys inspired by Starcraft to assign window to shift+ctrl+alt+cmd 1-8 and focus the window with ctrl+alt+cmd 1-8
 
 -- Usage:
---
+-- From ~/.hammerspoon/init.lua
+-- local windowhotkeys = require 'windowhotkeys'
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "1", "Assign Group 1", function()windowhotkeys.assign(1)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "2", "Assign Group 2", function()windowhotkeys.assign(2)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "3", "Assign Group 3", function()windowhotkeys.assign(3)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "4", "Assign Group 4", function()windowhotkeys.assign(4)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "5", "Assign Group 5", function()windowhotkeys.assign(5)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "6", "Assign Group 6", function()windowhotkeys.assign(6)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "7", "Assign Group 7", function()windowhotkeys.assign(7)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift" }, "8", "Assign Group 8", function()windowhotkeys.assign(8)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "1", "Focus Group 1", function()windowhotkeys.focus(1)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "2", "Focus Group 2", function()windowhotkeys.focus(2)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "3", "Focus Group 3", function()windowhotkeys.focus(3)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "4", "Focus Group 4", function()windowhotkeys.focus(4)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "5", "Focus Group 5", function()windowhotkeys.focus(5)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "6", "Focus Group 6", function()windowhotkeys.focus(6)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "7", "Focus Group 7", function()windowhotkeys.focus(7)end)
+-- hs.hotkey.bind({ "ctrl", "alt", "cmd"          }, "8", "Focus Group 8", function()windowhotkeys.focus(8)end)
 
 local windowhotkeys = {}
 
@@ -35,6 +52,7 @@ windowhotkeys.assign = function(input)
   local windowId = hs.window.focusedWindow():id()
   if windowId ~= nil then
     windowhotkeys.state[input] = windowId
+    windowhotkeys.persist()
   end
 end
 
@@ -44,6 +62,10 @@ windowhotkeys.focus = function(input)
     local window = hs.window.find(windowhotkeys.state[input])
     if window ~= nil then
       window:focus()
+    else
+      -- clear because we couldn't find window anymore
+      windowhotkeys.state[input] = nil
+      windowhotkeys.persist()
     end
   end
 end
