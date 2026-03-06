acf25ac356b1a130ce5dfd0f9911eb0ad7d9adf6
johnko <279736+johnko@users.noreply.github.com>
2026-03-06 04:26:03 -0500

    venv

--- bin/vm/ubuntu-create-autoinstall-iso.sh
+++ bin/vm/ubuntu-create-autoinstall-iso.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 set -eux
 
+VENV_FOLDER="venv-python3"
+
 ssh vm sudo apt update
 ssh vm sudo apt install -y git python3 python3-pip xorriso
 ssh vm sudo rm -fr livefs-editor
 ssh vm git clone https://github.com/johnko/livefs-editor.git
-ssh vm sudo python3 -m pip install ./livefs-editor/
+rsync -virchlmP "${HOME}/bin/setup-python3.sh" vm:./setup-python3.sh
+ssh vm sudo bash ./setup-python3.sh
+ssh vm sudo bash -c "source \"${VENV_PATH}/bin/activate\"; python3 -m pip install ./livefs-editor/"
 rsync -virchlmP "$(dirname "$0")/grub.cfg" vm:/tmp/grub.cfg
 rsync -virchlmP "${HOME}/iso/arm/ubuntu-24.04.4-live-server-arm64.iso" vm:./ubuntu-24.04.4-live-server-arm64.iso
-ssh vm sudo python3 -m livefs_edit ubuntu-24.04.4-live-server-arm64.iso autoinstall-24.04.2-live-server-arm64.iso --cp /tmp/grub.cfg new/iso/boot/grub/grub.cfg
+ssh vm sudo bash -c "source \"${VENV_PATH}/bin/activate\"; python3 -m livefs_edit ubuntu-24.04.4-live-server-arm64.iso autoinstall-24.04.2-live-server-arm64.iso --cp /tmp/grub.cfg new/iso/boot/grub/grub.cfg"
 rsync -virchlmP vm:./autoinstall-24.04.2-live-server-arm64.iso "${HOME}/iso/arm/autoinstall-24.04.2-live-server-arm64.iso"
