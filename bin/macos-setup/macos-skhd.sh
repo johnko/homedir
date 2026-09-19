83d967b7964a859ee3ab643285e69ef8ede8a24c
johnko <279736+johnko@users.noreply.github.com>
2026-09-19 15:28:04 -0400

    track latest commit hash for skhd mouse

--- bin/macos-setup/macos-skhd.sh
+++ bin/macos-setup/macos-skhd.sh
@@ -10,7 +10,7 @@ else
   set -x
   git clone -b mouse2 https://github.com/johnko/skhd.git /tmp/skhd.git
   pushd /tmp/skhd.git
-  git log -n 1 | grep be21d9b661dbd3c7371dba91bfc43387cabc911e || exit 1
+  git log -n 1 | grep 377762bde2e1ea732dcb9097e7c933cae3db60ac || exit 1
   # sudo xcode-select -switch /Library/Developer/CommandLineTools
   export CPATH="$(xcrun --show-sdk-path)/usr/include"
   export SDKROOT=$(xcrun --show-sdk-path)
